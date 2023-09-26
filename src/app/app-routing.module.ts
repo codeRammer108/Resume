1464aa89ecc8ffe210c280764ab4aa0a929ca269
Abhishek Sandliya <Abhishek.Sandliya@triconinfotech.com>
2023-09-26 23:18:54 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { EducationComponent } from './education/education.component';
+import { CompetitionComponent } from './competition/competition.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "competition",
+    component: CompetitionComponent
+  },
+  {
+    path: "education",
+    component: EducationComponent
+  },
+  {
+    path: "",
+    component: HomeComponent
+  },
+  { path: 'learning', loadChildren: () => import('./learning/learning.module').then(m => m.LearningModule) },
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
+import { CompetitionComponent } from './competition/competition.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    EducationComponent,
+    CompetitionComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
